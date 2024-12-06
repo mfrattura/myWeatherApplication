9721ffe6243bc67f38e01ec5b4e44ce9d31ffe08
Melissa Frattura <melissafrattura@gmail.com>
2024-12-06 11:30:16 +0800

    added time parse

--- src/index.js
+++ src/index.js
@@ -4,20 +4,48 @@ function refreshWeather(response) {
   let city = response.data.city;
   let humidity = response.data.temperature.humidity;
   let wind = response.data.wind.speed;
+  let condition = response.data.condition.description;
   let icon = response.data.condition.icon_url;
+  let date = new Date(response.data.time * 1000);
 
   // change the elements to the API Data
   let cityElement = document.querySelector("#city");
   let tempElement = document.querySelector("#temperature");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let conditionElement = document.querySelector("#condition");
   let iconElement = document.querySelector("#icon");
+  let timeElement = document.querySelector("#time");
 
   cityElement.innerHTML = city;
   tempElement.innerHTML = Math.round(temperature);
-  humidityElement.innerHTML = humidity;
-  windElement.innerHTML = wind;
+  humidityElement.innerHTML = `${humidity}%`;
+  windElement.innerHTML = `${wind}km/h`;
+  conditionElement.innerHTML = condition;
   iconElement.src = icon;
+  timeElement.innerHTML = formatDate(date);
+}
+
+function formatDate(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  if (minutes < 10) {
+    minutes == `0${minutes}`;
+  }
+
+  let day = days[date.getDay()];
+  return `${day},  ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
